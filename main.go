27e603c853dc3bfc186e5ec75756420727bb288f
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-01 20:11:44 -0300

    feat: created basic cache operation

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -28,6 +29,48 @@ func main() {
 		clientIP, port, _ := net.SplitHostPort(conn.RemoteAddr().String())
 		fmt.Println("Connection created " + clientIP + ":" + port)
 		conn.Read(data)
+		go handleOperation(data)
 		conn.Close()
+	}
+}
+
+func handleOperation(data []byte) {
+	message := string(data)
+	if strings.Contains(message, "Cache") {
+		handleCache(message)
+	} else if strings.Contains(message, "Uncache") {
+		handleUncache(message)
+	} else if strings.Contains(message, "Fetch") {
+		handleFetch(message)
+	} else {
+		fmt.Println("No operation")
+	}
+}
+func handleFetch(data string) {
+
+}
+func handleCache(message string) {
+	message = message[6:]
+	uuid := message[0:strings.IndexRune(message, ' ')]
+	data := []byte(message[strings.IndexRune(message, ' ')+1:])
+	fmt.Println("Uuid " + uuid)
+	fmt.Println("data" + string(data))
+
+	writeCache(uuid, data)
+
+}
+func handleUncache(data string) {
+
+}
+
+func writeCache(uuid string, data []byte) {
+
+	f, _ := os.ReadFile("cache.txt")
+
+	if f == nil {
+		file, _ := os.Create("cache.txt")
+
+		file.Write([]byte(uuid))
+		file.Sync()
 	}
 }
