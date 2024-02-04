fe8e7e3d75c8d31b208c7cd1d73b35de18cdfbd0
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-03 22:28:34 -0300

    feat: simplify command_handler

--- shell/command/command_handler.go
+++ shell/command/command_handler.go
@@ -8,15 +8,11 @@ import (
 )
 
 func CommandHandler(s string, conn net.Conn) {
-	if strings.Contains(s, "Cache") {
-		handleCache(s, conn)
-	} else if strings.Contains(s, "Fetch") {
-		handleFetch(s, conn)
-	} else if strings.Contains(s, "Uncache") {
-
-	} else if strings.Contains(s, "Exit") || strings.Contains(s, "exit") {
+	if strings.Contains(s, "Exit") || strings.Contains(s, "exit") {
 		conn.Close()
 		fmt.Println("Exit")
 		os.Exit(1)
 	}
+
+	conn.Write([]byte(s))
 }
