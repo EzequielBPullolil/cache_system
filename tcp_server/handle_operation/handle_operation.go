fa3318b324b119092221a071b02894594f786d9f
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-03 22:27:49 -0300

    fix: Fixed a bug that does not allow using the cache operation

--- tcp_server/handle_operation/handle_operation.go
+++ tcp_server/handle_operation/handle_operation.go
@@ -12,15 +12,13 @@ and returns a response if necessary
 */
 
 func HandleOperation(d []byte, response *string) {
-	data := strings.Split(string(d), "")
-	if len(data) > 2 {
+	data := strings.Split(string(d), " ")
+	if len(data) >= 2 {
 		operation := data[0]
 		uuid := data[1]
 		switch strings.ToLower(operation) {
 		case "cache":
-			if len(data) > 3 {
-				cacheManager.WriteCache(uuid, data[2])
-			}
+			cacheManager.WriteCache(uuid, data[2])
 		case "uncache":
 			cacheManager.Uncache(uuid)
 		case "fetch":
