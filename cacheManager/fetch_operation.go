dc2daa23d3fcc9f71d3d8853644aac82a72ee670
EzequielBPullolil <programacionezequielk@gmail.com>
2024-02-04 07:32:14 -0300

    feat: Implemented fetch operation

--- cacheManager/fetch_operation.go
+++ cacheManager/fetch_operation.go
@@ -1,6 +1,20 @@
 package cacheManager
 
+import (
+	"os"
+	"strings"
+)
+
+// Iterate the file finding an uuid equal to 'uuid'
 func FetchCache(uuid string) (string, error) {
-	return "", nil
+	d, _ := os.ReadFile(os.Getenv("cache_file"))
+	data := ""
+	file_data := strings.Split(string(d), "\n")
+	for i := 0; i < len(file_data); i++ {
+		if file_data[i] == "["+uuid+"]" {
+			data = file_data[i]
+		}
+	}
+	return data, nil
 
 }
